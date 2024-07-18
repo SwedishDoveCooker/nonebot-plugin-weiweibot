411248255d645a51cf097d5bef1a7a57a765a0d2
J4ckacc <85827122+SwedishDoveCooker@users.noreply.github.com>
2024-07-18 18:24:20 +0800

    Update config.py

--- nonebot_plugin_weiweibot/config.py
+++ nonebot_plugin_weiweibot/config.py
@@ -1,5 +1,5 @@
 from datetime import timedelta
 from pydantic import BaseModel, Field
 
-class MyModel(BaseModel):
+class Config(BaseModel):
     SESSION_EXPIRE_TIMEOUT: timedelta = Field(default=timedelta(seconds=30))
